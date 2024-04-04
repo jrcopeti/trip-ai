a3247cc09a36bea7cd6da1b835321dfcd7f59a0d
Jose Copeti <jrcopeti@gmail.com>
2024-04-04 12:56:35 +0200

    refactor form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -27,13 +27,19 @@ import { Controller, useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useTrip } from "@/hooks/useTrip";
+import { useImage } from "@/hooks/useImage";
 
 import { useWeather } from "@/hooks/useWeather";
 import { useFormData } from "@/hooks/useFormData";
 import { createTripInDB } from "@/db/actions";
 import type { Trip } from "@prisma/client";
-
-import { useImage } from "@/hooks/useImage";
+import {
+  sortedTypes,
+  luggageSizes,
+  sortedAccommodations,
+  budgets,
+  sortedInterest,
+} from "@/data";
 
 type Inputs = z.infer<typeof FormDataSchema>;
 
@@ -88,80 +94,6 @@ const steps = [
     stepValue: 100,
     fields: [],
   },
-];
-
-const types = [
-  { value: "family", label: "Family" },
-  { value: "adventure", label: "Adventure" },
-  { value: "romantic", label: "Romantic" },
-  { value: "solo", label: "Solo" },
-  { value: "business", label: "Business" },
-  { value: "cultural", label: "Cultural" },
-  { value: "festival", label: "Festival" },
-  { value: "gastronomy", label: "Gastronomy" },
-  { value: "nature", label: "Nature" },
-];
-const sortedTypes = [...types.sort((a, b) => a.label.localeCompare(b.label))];
-
-const luggageSizes = [
-  { value: "backpack", label: "Backpack" },
-  { value: "carry-on", label: "carry-on" },
-  { value: "medium", label: "Medium" },
-  { value: "large", label: "Large" },
-  { value: "extra-large", label: "Extra Large" },
-];
-
-const accommodations = [
-  { value: "hotel", label: "Hotel" },
-  { value: "private acommodation", label: "Private Accomodation" },
-  { value: "hostel", label: "Hostel" },
-  { value: "apartment", label: "Apartment" },
-  { value: "friend's house", label: "Friend's Place" },
-  { value: "bed and breakfast", label: "Bed and Breakfast" },
-  { value: "resort", label: "Resort" },
-  { value: "cabin", label: "Cabin" },
-  { value: "camping", label: "Camping" },
-];
-const sortedAccommodations = [
-  ...accommodations.sort((a, b) => a.label.localeCompare(b.label)),
-];
-
-const budgets = [
-  { value: "low", label: "Low Budget" },
-  { value: "comfort", label: "Comfort" },
-  { value: "luxury", label: "Luxury" },
-];
-
-const interests = [
-  { value: "mountain", label: "Mountain" },
-  { value: "city", label: "City" },
-  { value: "culture", label: "Culture" },
-  { value: "food", label: "Food" },
-  { value: "history", label: "History" },
-  { value: "nature", label: "Nature" },
-  { value: "shopping", label: "Shopping" },
-  { value: "art", label: "Art" },
-  { value: "wildlife", label: "Wildlife" },
-  { value: "nightlife", label: "Beach" },
-  { value: "photography", label: "Photography" },
-  { value: "museums", label: "Museums" },
-  { value: "wine", label: "wine" },
-  { value: "coffee", label: "Coffee" },
-  { value: "wellness", label: "Wellness" },
-  { value: "dating", label: "Dating" },
-  { value: "music", label: "Music" },
-  { value: "hiking", label: "Hiking" },
-  { value: "surfing", label: "Susports" },
-  { value: "sports", label: "Sports" },
-  { value: "cinema", label: "Cinema" },
-  { value: "dance", label: "Dance" },
-  { value: "books", label: "Books" },
-  { value: "politics", label: "Politics" },
-  { value: "architecture", label: "Architecture" },
-  { value: "games", label: "Games" },
-];
-const sortedInterest = [
-  ...interests.sort((a, b) => a.label.localeCompare(b.label)),
 ];
 
 function Form() {
---
+++ src/data/index.ts
@@ -0,0 +1,76 @@
+
+ const types = [
+  { value: "family", label: "Family" },
+  { value: "adventure", label: "Adventure" },
+  { value: "romantic", label: "Romantic" },
+  { value: "solo", label: "Solo" },
+  { value: "business", label: "Business" },
+  { value: "cultural", label: "Cultural" },
+  { value: "festival", label: "Festival" },
+  { value: "gastronomy", label: "Gastronomy" },
+  { value: "nature", label: "Nature" },
+];
+ const sortedTypes = [...types.sort((a, b) => a.label.localeCompare(b.label))];
+
+const luggageSizes = [
+  { value: "backpack", label: "Backpack" },
+  { value: "carry-on", label: "carry-on" },
+  { value: "medium", label: "Medium" },
+  { value: "large", label: "Large" },
+  { value: "extra-large", label: "Extra Large" },
+];
+
+const accommodations = [
+  { value: "hotel", label: "Hotel" },
+  { value: "private acommodation", label: "Private Accomodation" },
+  { value: "hostel", label: "Hostel" },
+  { value: "apartment", label: "Apartment" },
+  { value: "friend's house", label: "Friend's Place" },
+  { value: "bed and breakfast", label: "Bed and Breakfast" },
+  { value: "resort", label: "Resort" },
+  { value: "cabin", label: "Cabin" },
+  { value: "camping", label: "Camping" },
+];
+const sortedAccommodations = [
+  ...accommodations.sort((a, b) => a.label.localeCompare(b.label)),
+];
+
+const budgets = [
+  { value: "low", label: "Low Budget" },
+  { value: "comfort", label: "Comfort" },
+  { value: "luxury", label: "Luxury" },
+];
+
+const interests = [
+  { value: "mountain", label: "Mountain" },
+  { value: "city", label: "City" },
+  { value: "culture", label: "Culture" },
+  { value: "food", label: "Food" },
+  { value: "history", label: "History" },
+  { value: "nature", label: "Nature" },
+  { value: "shopping", label: "Shopping" },
+  { value: "art", label: "Art" },
+  { value: "wildlife", label: "Wildlife" },
+  { value: "nightlife", label: "Beach" },
+  { value: "photography", label: "Photography" },
+  { value: "museums", label: "Museums" },
+  { value: "wine", label: "wine" },
+  { value: "coffee", label: "Coffee" },
+  { value: "wellness", label: "Wellness" },
+  { value: "dating", label: "Dating" },
+  { value: "music", label: "Music" },
+  { value: "hiking", label: "Hiking" },
+  { value: "surfing", label: "Susports" },
+  { value: "sports", label: "Sports" },
+  { value: "cinema", label: "Cinema" },
+  { value: "dance", label: "Dance" },
+  { value: "books", label: "Books" },
+  { value: "politics", label: "Politics" },
+  { value: "architecture", label: "Architecture" },
+  { value: "games", label: "Games" },
+];
+const sortedInterest = [
+  ...interests.sort((a, b) => a.label.localeCompare(b.label)),
+];
+
+export { sortedTypes, luggageSizes, sortedAccommodations, budgets, sortedInterest };
