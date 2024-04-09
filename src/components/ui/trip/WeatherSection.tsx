64d3edca6e40b42f1e5e2c5d91f3896f98bf3e31
Jose Copeti <jrcopeti@gmail.com>
2024-04-09 11:41:09 +0200

    weather card

--- src/components/ui/trip/WeatherSection.tsx
+++ src/components/ui/trip/WeatherSection.tsx
@@ -18,7 +18,7 @@ function WeatherSection({
 }: WeatherSectionProps) {
   return (
     <>
-      <Card className="py-4">
+      {/* <Card className="py-4">
         <CardHeader className="flex-col items-start px-4 pb-0 pt-2">
           <p className="text-tiny font-bold uppercase">{temperature}ºC</p>
           <small className="text-default-500">
@@ -57,7 +57,28 @@ function WeatherSection({
             </CardHeader>
           </div>
         </CardBody>
-      </Card>
+      </Card> */}
+      <div className="mx-0 my-[2.5rem] w-full max-w-[40.625rem] flex-col items-center rounded-[24px] bg-gallery-100 px-[50px] py-[3.125rem] text-center sm:mx-0 sm:my-0 sm:w-[75dvw] sm:flex-row sm:pl-[1.25rem] sm:pr-[1.875rem] sm:text-start">
+        <Image
+          src={weatherIconSrc}
+          alt="weather-icon"
+          width={100}
+          height={100}
+          className="h-[45vw] w-[45vw] rounded-[50%] -mt-[140px] mb-[30px] ml-[65px]  "
+        />
+
+        <div>
+          <h2 className="mb-[0.65rem] mr-0 mt-0 text-2xl font-semibold sm:mr-[1.875rem] sm:text-3xl">
+            Kaye Morris
+          </h2>
+          <h3 className="text-lg">UX Developer</h3>
+          <p className="text-md m-0 font-semibold opacity-70 sm:max-w-[360px]">
+            Empowering users through captivating interfaces, turning ideas into
+            pixel-perfect realities.
+          </p>
+          <small>max min</small>
+        </div>
+      </div>
     </>
   );
 }
