500e71a236320f15d067a9906e3d08fde0f8a7c7
Jose Copeti <jrcopeti@gmail.com>
2024-04-16 23:53:38 +0200

    homepage

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -6,15 +6,15 @@ function FormPage() {
   return (
     <>
       <div className="tours-section relative flex h-screen items-center justify-center overflow-hidden ">
-        <div className="absolute left-0 top-0 -z-20 h-full w-full bg-gradient-to-b from-gallery-100 to-gallery-200  "></div>
+        <div className="absolute left-0 top-0 -z-20 h-full w-full bg-gradient-to-b from-gallery-50 to-gallery-100  "></div>
         <div className="absolute inset-0 -left-[800px] -top-[192px] flex justify-center">
-          <div className="ml-[8rem] h-[520px] w-[520px] rounded-full bg-gradient-radial from-violay-500 opacity-50 blur-[150px] filter md:blur-[80px]"></div>
-          <div className="mr-[8rem] h-[520px] w-[520px] rounded-full bg-deeporange-500 opacity-50  mix-blend-color-burn blur-[150px] filter md:blur-[150px]  "></div>
+          <div className="ml-[8rem] h-[520px] w-[520px] rounded-full bg-gradient-radial from-violay-500 opacity-50 blur-[60px] filter"></div>
+          <div className="mr-[8rem] h-[520px] w-[520px] rounded-full bg-deeporange-500 opacity-50  mix-blend-color-burn blur-[60px] filter  "></div>
           {/* <div className="h-[520px] w-[520px] rounded-full from bg-gradient-conic mix-blend-color-burn  from-violay-500 to-deeporange-500 opacity-50 blur-[30px] filter  "></div> */}
         </div>
         <div className="absolute inset-0 left-[35%] top-[80%] flex justify-center">
-          <div className="ml-[8rem] h-[520px] w-[520px] rounded-full bg-violay-500 opacity-60  blur-[150px] filter md:blur-[150px] "></div>
-          <div className="mr-[8rem] h-[520px] w-[520px] rounded-full bg-deeporange-500 opacity-50  mix-blend-color-burn blur-[150px] filter md:blur-[150px]  "></div>
+          <div className="ml-[8rem] h-[520px] w-[520px] rounded-full bg-violay-500 opacity-60  blur-[150px] filter md:blur-[100px] "></div>
+          <div className="mr-[8rem] h-[520px] w-[520px] rounded-full bg-deeporange-500 opacity-50  mix-blend-color-burn blur-[150px] filter md:blur-[100px]  "></div>
         </div>
 
         <Form />
--- src/components/ui/HomepageComponent.tsx
+++ src/components/ui/HomepageComponent.tsx
@@ -9,22 +9,17 @@ import image4 from "@/assets/4.jpg";
 import Image from "next/image";
 
 function HomepageComponent() {
-  const useIsomorphicLayoutEffect =
-    typeof window !== "undefined" ? useLayoutEffect : useEffect;
-
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll" as any))
-        .default;
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
-
   return (
     <>
       <section className="relative flex h-screen items-center justify-center">
-        <div className="grid h-full w-full grid-cols-none grid-rows-2 bg-slate-400 lg:grid-cols-2 lg:grid-rows-none ">
-          <div className="relative h-full w-full bg-slate-600">title</div>
+        <div className="grid h-full w-full grid-cols-none grid-rows-2 lg:grid-cols-2 lg:grid-rows-none ">
+          <div className="relative h-full w-full bg-gallery-50">
+            <aside className="absolute inset-0 -left-[800px] -top-[192px] flex justify-center">
+              <div className="ml-[8rem] h-[520px] w-[520px] rounded-full bg-violay-500 opacity-60  blur-[150px] filter md:blur-[100px] "></div>
+              <div className="mr-[8rem] h-[520px] w-[520px] rounded-full bg-deeporange-500 opacity-50  mix-blend-color-burn blur-[150px] filter md:blur-[100px]  "></div>
+              {/* <div className="h-[520px] w-[520px] rounded-full from bg-gradient-conic mix-blend-color-burn  from-violay-500 to-deeporange-500 opacity-50 blur-[30px] filter  "></div> */}
+            </aside>
+          </div>
           <div className="relative h-full w-full bg-red-100">
             <Image
               src={image3.src}
@@ -40,3 +35,21 @@ function HomepageComponent() {
 }
 
 export default HomepageComponent;
+
+// <div className="grid h-[90%] w-[90%] grid-cols-none grid-rows-2 lg:h-[80%] lg:w-[80%] lg:grid-cols-2 lg:grid-rows-none ">
+//   <div className="relative h-full w-full ">
+//     <Image
+//       src={image1}
+//       alt="city"
+//       priority
+//       fill
+//       className="object-cover shadow-xl"
+//     />
+//   </div>
+
+//   <div className="bg-gallery-50/40 p-8 shadow-sm sm:p-16">
+//     <h1 className="ml-4 mt-8 text-4xl font-extrabold text-tuna-900 sm:ml-8 sm:mt-3 sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-7xl">
+//       HOMEPAGE
+//     </h1>
+//   </div>
+// </div>
