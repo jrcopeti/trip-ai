a688f44df59f8cb693c60d580b5c1dccddd26e06
Jose Copeti <jrcopeti@gmail.com>
2024-04-17 12:49:09 +0200

    slider

---
+++ src/components/ui/Slider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { homepageImages } from "@/data";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Image from "next/image";
+
+function Slider() {
+  return (
+    <section >
+      <Swiper
+        navigation
+        pagination={{ type: "fraction" }}
+        modules={[Navigation, Pagination]}
+        className="h-full w-full"
+      >
+        {homepageImages.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className='relative h-full w-full'>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                className="block h-full w-full object-cover"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+}
+
+export default Slider;
