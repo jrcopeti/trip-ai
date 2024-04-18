b8ff3da7e99c0962a95c53d9bbae79bb9ffae6dd
Jose Copeti <jrcopeti@gmail.com>
2024-04-18 16:33:13 +0200

    gradients

--- src/components/ui/Gradient1.tsx
+++ src/components/ui/Gradient1.tsx
@@ -1,23 +1,21 @@
 interface Gradient1Props {
   left: string;
   top: string;
-  bgColor1: string;
-  bgColor2: string;
-  blur: number;
-  mdBlur: number;
+  color1: string;
+  color2: string;
+  blur: string;
 }
 
-function Gradient1({ left, top, bgColor1, bgColor2, blur, mdBlur }: Gradient1Props) {
+function Gradient1({ left, top, color1, color2, blur }: Gradient1Props) {
   return (
     <section
-    style={{ top: top, left: left }}
-    className={`absolute inset-0 flex justify-center z-20`}
+      className={`absolute inset-0 z-20 flex justify-center ${top} ${left}`}
     >
       <div
-        className={`ml-[8rem] h-[520px] w-[520px] rounded-full ${bgColor1} opacity-60  blur-[${blur}px] filter md:blur-[${mdBlur}px]`}
+        className={`ml-[8rem] h-[520px] w-[520px] rounded-full ${color1} opacity-60 ${blur} filter`}
       ></div>
       <div
-        className={`mr-[8rem] h-[520px] w-[520px] rounded-full ${bgColor2} opacity-50  mix-blend-color-burn blur-[${blur}px] filter md:blur-[${mdBlur}px]`}
+        className={`mr-[8rem] h-[520px] w-[520px] rounded-full ${color2} opacity-50  mix-blend-color-burn ${blur} filter`}
       ></div>
     </section>
   );
