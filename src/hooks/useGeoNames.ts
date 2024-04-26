07a3d816cde54b12f333cea16222a25629fe66a0
Jose Copeti <jrcopeti@gmail.com>
2024-04-26 23:38:02 +0200

    geonames api

--- src/hooks/useGeoNames.ts
+++ src/hooks/useGeoNames.ts
@@ -1,6 +1,6 @@
 "use client";
-import axios from "axios";
 import { useEffect, useState } from "react";
+import Geonames from "geonames.js";
 import type { useGeoNamesProps, GeoName } from "@/types";
 
 export function useGeoNames({ city, countryCode }: useGeoNamesProps) {
@@ -14,49 +14,49 @@ export function useGeoNames({ city, countryCode }: useGeoNamesProps) {
       setErrorCityValid("");
       return;
     }
-
-    const source = axios.CancelToken.source();
     const username = process.env.NEXT_PUBLIC_GEONAMES_USERNAME;
-    const url = `http://api.geonames.org/searchJSON?q=${encodeURIComponent(city)}&country=${countryCode}&maxRows=10&featureClass=P&username=${username}`;
-    const debounce = setTimeout(async function validateCityCountry() {
-      setIsCityValid(false);
+    const geonames = Geonames({
+      username: username,
+      lan: "en",
+      encoding: "JSON",
+    });
+    const debounce = setTimeout(() => {
       setIsLoadingCityValid(true);
+      setIsCityValid(false);
       setErrorCityValid("");
-      axios
-        .get(url, { cancelToken: source.token })
+      geonames
+        .search({
+          q: city,
+          country: countryCode,
+          maxRows: 5,
+          featureClass: "P",
+        })
         .then((response) => {
-          if (response.data.totalResultsCount === 0) {
-            throw new Error("Location is not valid. Please try again.");
+          if (response.totalResultsCount === 0) {
+            throw new Error("No location was found. Please try again.");
           }
-
-          const validCity = response.data.geonames.find((geo: GeoName) => {
+          const validCity = response.geonames.find((geo: GeoName) => {
             const formattedCity = city.trim().toLowerCase();
             const formattedGeoName = geo.name.trim().toLowerCase();
             return formattedGeoName === formattedCity && geo.population > 1;
           });
-
           console.log("Valid City", validCity);
           if (!validCity) {
             throw new Error("Location is not valid. Please try again.");
           } else {
             console.log(
               "City and countryCode match found",
-              response.data,
-              response.data.geonames[0],
+              response,
+              response.geonames[0],
             );
             setIsCityValid(true);
             setErrorCityValid("");
           }
         })
-
         .catch((error) => {
-          if (axios.isCancel(error)) {
-            console.log("Request canceled", error.message);
-          } else {
-            console.error("Error Validating City", error);
-            setIsCityValid(false);
-            setErrorCityValid(error.message);
-          }
+          console.error("Error Validating City", error);
+          setIsCityValid(false);
+          setErrorCityValid(error.message || "An error occurred");
         })
         .finally(() => {
           setIsLoadingCityValid(false);
@@ -64,7 +64,6 @@ export function useGeoNames({ city, countryCode }: useGeoNamesProps) {
     }, 500);
     return () => {
       clearTimeout(debounce);
-      source.cancel("Request canceled by the user");
     };
   }, [city, countryCode]);
 
