c6e5762efb949fc5d8a02d333dfeebbc14789981
Jose Copeti <jrcopeti@gmail.com>
2024-04-27 02:38:41 +0200

    api folder

--- src/app/api/responseAI/router.ts
+++ src/app/api/responseAI/router.ts
@@ -4,26 +4,8 @@ import { fetchResponseAI } from "./openaiApi";
 export const maxDuration = 120;
 export const dynamic = "force-dynamic";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method === "POST") {
-    const { prompt } = req.body;
-    if (!prompt) {
-      res.status(400).json({ error: "Prompt is required" });
-      return;
-    }
-
-    try {
-      const aiResponse = await fetchResponseAI(prompt);
-      res.status(200).json(aiResponse);
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Failed to fetch response from AI" });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { prompt } = req.body;
+  const response = await fetchResponseAI(prompt);
+  res.status(200).json(response);
 }
