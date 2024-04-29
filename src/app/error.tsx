691d0f27af8e159b3745f1c6644baf0ad8a02add
Jose Copeti <jrcopeti@gmail.com>
2024-04-29 23:38:53 +0200

    homepage style

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,15 +1,10 @@
 "use client";
-
-import { Button } from "@nextui-org/react";
-import { useEffect } from "react";
-import { MdOutlineErrorOutline } from "react-icons/md";
-import NotFound from "./not-found";
 import NotFoundComponent from "@/components/ui/NotFoundComponent";
 
 export default function Error() {
   return (
     <NotFoundComponent
-      message="Something went wrong."
+      message="Something went wrong!"
       path="/"
       button="Back to Home"
     />
