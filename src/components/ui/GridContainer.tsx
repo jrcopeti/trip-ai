006e070dd6fd5236529ba47cfca516098eeb6101
Jose Copeti <jrcopeti@gmail.com>
2024-04-30 10:21:37 +0200

    styles

--- src/components/ui/GridContainer.tsx
+++ src/components/ui/GridContainer.tsx
@@ -3,7 +3,7 @@ import type { GridContainerProps } from "@/types";
 function GridContainer({ children, bg }: GridContainerProps) {
   return (
     <div
-      className={`z-30 grid h-[90%] w-[90%] grid-cols-1 grid-rows-2 gap-6 overflow-hidden ${bg} shadow-xl lg:h-[80%] lg:w-[80%] lg:grid-cols-2 lg:grid-rows-none`}
+      className={`z-30 grid h-[90%] w-[90%] grid-cols-1 grid-rows-2 gap-6 overflow-auto ${bg} shadow-xl lg:h-[80%] lg:w-[80%] lg:grid-cols-2 lg:grid-rows-none`}
     >
       {children}
     </div>
