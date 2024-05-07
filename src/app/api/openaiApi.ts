5573cd52cbc164dddf986ffeacd7393bee2a2e68
Jose Copeti <jrcopeti@gmail.com>
2024-05-08 01:21:57 +0200

    refactor styles

--- src/app/api/openaiApi.ts
+++ src/app/api/openaiApi.ts
@@ -95,7 +95,7 @@ const functionData = {
 export const fetchResponseAI = async (prompt: string) => {
   try {
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4",
       messages: [
         { role: "system", content: systemInstructions },
         { role: "user", content: prompt },
