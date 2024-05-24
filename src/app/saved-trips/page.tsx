6d6549e5f453bbef09aec01caa31e808b112734a
Jose Copeti <jrcopeti@gmail.com>
2024-05-24 08:36:40 +0200

    refactor saved trips display

--- src/app/saved-trips/page.tsx
+++ src/app/saved-trips/page.tsx
@@ -1,18 +1,26 @@
-import SavedTripsDisplay from "@/components/trips/SavedTripsDisplay";
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
+import SavedTripsDisplay from "@/components/trips/SavedTripsDisplay";
+import Container from "@/components/ui/Container";
+import GradientBg from "@/components/ui/GradientBg";
+import SavedTripsContainer from "@/components/trips/SavedTripsContainer";
 
 function SavedTripsPage() {
   const queryClient = new QueryClient();
 
   return (
     <>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <SavedTripsDisplay />
-      </HydrationBoundary>
+      <Container height="h-full">
+        <GradientBg position="fixed" />
+        <SavedTripsContainer>
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            <SavedTripsDisplay />
+          </HydrationBoundary>
+        </SavedTripsContainer>
+      </Container>
     </>
   );
 }
